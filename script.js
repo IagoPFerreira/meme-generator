b4432ebdebeeaeda427e54d0d9292b226c50dbea
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-20 13:24:12 -0300

    Segundo requisito

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ function showText() {
   });
 }
 
+function showImagem() {
+  const inputImage = document.querySelector('#meme-insert');
+  inputImage.addEventListener('change', (e) => {
+    const memeContainer = document.querySelector('#meme-image-container');
+    const newImage = document.createElement('img');
+    newImage.id = 'memeImage';
+    memeContainer.appendChild(newImage);
+    newImage.src = URL.createObjectURL(e.target.files[0]);
+    newImage.onload = () => {
+      URL.revokeObjectURL(newImage.src);
+    };
+  });
+}
+
 window.onload = () => {
   showText();
+  showImagem();
 };
