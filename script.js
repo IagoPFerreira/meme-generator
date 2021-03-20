0582966970e78724a96a27b8aa9ac9d3c92223fa
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-20 16:36:37 -0300

    Correcao do requisito 3 v.2

--- script.js
+++ script.js
@@ -8,13 +8,12 @@ function showText() {
   inputText.appendChild(newText);
 }
 
+// Refatoração com considerações de Wanderson Salles Turma 10 Tribo A
 function showImagem() {
   const inputImage = document.querySelector('#meme-insert');
+
   inputImage.addEventListener('change', (e) => {
-    const memeContainer = document.querySelector('#meme-image');
-    const newImage = document.createElement('img');
-    newImage.id = 'memeImage';
-    memeContainer.appendChild(newImage);
+    const newImage = document.querySelector('#memeImage');
     newImage.src = URL.createObjectURL(e.target.files[0]);
     newImage.onload = () => {
       URL.revokeObjectURL(newImage.src);
@@ -31,20 +30,20 @@ waterBtn.style.backgroundColor = 'rgb(0, 0, 255)';
 const earthBtn = document.querySelector('#earth');
 earthBtn.style.backgroundColor = 'rgb(0, 128, 0)';
 
-const memeImageContainer = document.querySelector('#meme-image-container');
+const memeContainer = document.querySelector('#meme-image');
 
 console.log(fireBtn, waterBtn, earthBtn);
 
 fireBtn.addEventListener('click', () => {
-  memeImageContainer.style.border = 'red dashed 3px';
+  memeContainer.style.border = 'red dashed 3px';
 });
 
 waterBtn.addEventListener('click', () => {
-  memeImageContainer.style.border = 'blue double 5px';
+  memeContainer.style.border = 'blue double 5px';
 });
 
 earthBtn.addEventListener('click', () => {
-  memeImageContainer.style.border = 'green groove 6px';
+  memeContainer.style.border = 'green groove 6px';
 });
 
 window.onload = () => {
